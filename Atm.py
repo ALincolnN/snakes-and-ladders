c9feac8783121a055bd191dba351c3047308be84
ALincolnN <njoguabraham2@gmail.com>
2022-09-27 08:45:47 +0300

    Added the documentation of this project and also fixed the input instructions

--- Atm.py
+++ Atm.py
@@ -1,65 +1,8 @@
 '''
+The project aims at getting the input from the user with clear instruction that it should be 12 characters long.
 
-def mask(cvv):
-    cvv.replace(cvv[1],'x')
-    print(cvv)
-
-
-mask(input('Enter your Credit Card Number: '))
-
-
-for i in a:
-    i.replace(i,'x')
-    print(i)
-
-
-a = ('My name is lincoln',1)
-
-#a.replace(a[0],'x')
-
-print(a[0][0])
-
-txt = "one one was a race horse, two two was one too."
-
-x=txt.replace("one", "three")
-
-print(x)
-
-
-a = 'My name is lincoln'
-
-b=a.replace(a[0],'x')
-
-print(b)
-
-
-def mask(cvv):
-    conv = ''
-    for i in cvv:
-        conv=cvv.replace(i,'*')
-
-
-
-
-    print(conv)
-
-
-
-
-mask(input('Enter your Credit Card Number: '))
-
-
-
-from statistics import mode
-
-a = [1,1,1,2,3,6,1,4]
-b = mode(a)
-# for i in a:
-#
-#     #i == '*'
-#
-#     b.append(i)
-print(b)
+The function mask() then converts the first 8 characters of the string and replaces them with asterisks and only
+    reveals the last 4
 '''
 def mask(cvv):
 
@@ -75,7 +18,7 @@ def mask(cvv):
         err = 'The required length should be 12 characters!! TRY AGAIN!!'
         return err
 
-code = input('Enter your Credit Card Number: ')
+code = input('Enter your 12 digit Credit Card Number: ')
 print(mask(code))
 
 
