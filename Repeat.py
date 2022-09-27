168d9134e5223d4fe24630f42d8635a88fad2c1f
ALincolnN <njoguabraham2@gmail.com>
2022-09-27 09:42:30 +0300

    Updated the documentation of this project.

--- Repeat.py
+++ Repeat.py
@@ -1,17 +1,7 @@
-'''
-a='123l'
-
-b=''
-
-for i in a:
-    b += i+i
-
-print(b)
 
 '''
-
-'''
-The aim of this coding challenge is to have a function that doubles each and every character in a string that the user inputs
+The aim of this coding challenge is to have a function that doubles each and every character in a string that the user
+    inputs using the repeat() function.
 '''
 
 def repeat(whatever):
