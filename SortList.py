bfb3496b0c2b814e51972727dd34867efc40a7a4
ALincolnN <njoguabraham2@gmail.com>
2022-09-27 09:48:45 +0300

    Added the documentation of this project and updated the input options for the user to follow.

--- SortList.py
+++ SortList.py
@@ -1,5 +1,9 @@
 
-
+'''
+The project is aimed at taking user inputs like the option to use for sorting, number of times the user should input
+    their desired values and then the items to include in the list according to the number of times specified.
+    The program will then sort the list according to the option the user has entered.
+'''
 
 def SortList(opt,l=0):
 
@@ -24,8 +28,8 @@ def SortList(opt,l=0):
 
     else:
         print('Wrong Sort Option!!')
-
-SortList(input('Enter an option for sorting: '))
+option = input('Enter an option for sorting i.e(desc,asc or none): ')
+SortList(option)
 
 
 
