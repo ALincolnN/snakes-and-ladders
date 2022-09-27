527b1fd717c330a668a2765385cc9035636e7c9d
ALincolnN <njoguabraham2@gmail.com>
2022-09-27 09:39:52 +0300

    Added the documentation of this project.

--- LessThan10.py
+++ LessThan10.py
@@ -1,4 +1,7 @@
-
+'''
+The project aims at getting an integer list as an input,checking the items in the list and if the item is less than 5,it appends
+    the item to another list which will be displayed after the iteration.
+'''
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 
 b = []
