2d324e4868b1a6569551c0a7e1eaea5759df5da8
ALincolnN <njoguabraham2@gmail.com>
2022-09-27 09:32:44 +0300

    Added the documentation of this project and wrapped the workings in a function

--- InvertList.py
+++ InvertList.py
@@ -1,23 +1,20 @@
 '''
-import numpy as np
+The projects aims at taking a string, converting the string to a list, breaking the list into chunks of three and
+    finally reversing each list returned and printing it using the invert() function.
 
-a = [1,2,3,4,5,6,7,8,9,10,11]
-
-b = np.array_split(a,3)
-
-for i in b:
-    print(list(i))
 '''
 
 my_list = [1, 2, 3, 4, 5,
            6, 7, 8, 9,11,12,'l',5,4,5]
 
 
-for i in range(0, len(my_list),3):
+def invert(l):
+    for i in range(0, len(l),3):
 
-    y=my_list[i:i+3]
-    y.reverse()
+        y=my_list[i:i+3]
+        y.reverse()
 
-    print(y)
+        print(y)
 
-
+my_list = list(input('Please enter a string or characters in any desired order: '))
+invert(my_list)
