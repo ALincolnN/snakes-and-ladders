e8dc98d61be2e6e6d1733529b0d87c202d76306f
ALincolnN <njoguabraham2@gmail.com>
2022-09-27 09:25:02 +0300

    Added the documentation of this project

--- discount.py
+++ discount.py
@@ -1,4 +1,8 @@
-
+'''
+Using the discount() function, this project is aimed at getting the user input on the marked price of an item, the
+    percentage discount to be applied and finally returns the calculated selling price of the item and the discount
+    applied.
+'''
 def discount(mp,d):
     sp = mp*((100-d)/100)
     print('\nThe price after discount is: '+str(sp))
